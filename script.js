c15eeefd2ff9f3c7310715416262430df37ed902
Mirko Moschella <shary.mirko@gmail.com>
2022-03-07 14:49:21 +0100

    esercizio completato! tocca al bonus

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 /*Viene fornito un layout di base. Come prima cosa nel file js definitevi un array di oggetti che rappresentano i membro del team (prendete i dati dallo screen fornito). Ogni membro deve avere le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
 Prendendo come riferimento la card di esempio presente nell'html, stampiamo dinamicamente una card per ogni membro del team.*/
-
-
+const teamCardContainer = document.querySelector('.team-container');
 
 const arrTeamMember = [
     {
@@ -34,5 +33,21 @@ const arrTeamMember = [
         role: 'Graphic Designer',
         picture: 'barbara-ramos-graphic-designer.jpg'
     }
-    
-]
+];
+
+for (let i = 0; i < arrTeamMember.length; i++){
+    teamCardContainer.innerHTML += `
+    <div class="team-card">
+            <div class="card-image">
+              <img
+                src="img/${arrTeamMember[i].picture}"
+                alt="${arrTeamMember[i].name}"
+              />
+            </div>
+            <div class="card-text">
+              <h3>${arrTeamMember[i].name}</h3>
+              <p>${arrTeamMember[i].role}</p>
+            </div>
+          </div>
+    `
+}
